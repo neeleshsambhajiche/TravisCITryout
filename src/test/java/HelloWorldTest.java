7f7cb25b860180004eabc70ca8379542e9856a1f
Neelesh Sambhajiche <neelesh.sa@247.ai>
2019-09-16 21:45:03 +0530

    Added test cases

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @since 16/09/19
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void testCalculateAppriasal() {
+        assertEquals(500, 400);
+    }
+}
